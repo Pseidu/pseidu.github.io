da07ddda3a7b2a3ea24697196d05385e5580fa27
Rafael Soriano <rsoriano.insti@gmail.com>
2022-11-10 20:55:00 +0100

    10-11-22

--- js/moveSlider.js
+++ js/moveSlider.js
@@ -2,20 +2,20 @@ function nextImage(){
     //Get all images (slides) in carousel
     const elements = document.getElementsByClassName("slide");
     //Move elements forward
-    let auxElement = elements[0].src;
+    let auxElement = elements[0].outerHTML;
     for (let index = 0; index < (elements.length-1); index++) {
-        elements[index].src = elements[index+1].src;
+        elements[index].outerHTML = elements[index+1].outerHTML;
     }
-    elements[(elements.length-1)].src = auxElement;
+    elements[(elements.length-1)].outerHTML = auxElement;
 }
 
 function prevImage(){
     //Get all images (slides) in carousel
     const elements = document.getElementsByClassName("slide");
     //Move elements backward
-    let auxElement = elements[(elements.length-1)].src;
+    let auxElement = elements[(elements.length-1)].outerHTML;
     for (let index = (elements.length-1); index > 0; index--) {
-        elements[index].src = elements[index-1].src;
+        elements[index].outerHTML = elements[index-1].outerHTML;
     }
-    elements[0].src = auxElement;
+    elements[0].outerHTML = auxElement;
 }
