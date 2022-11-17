af5e875251434de9f69c15bd08dc18c080086b5b
Rafael Soriano <rsoriano.insti@gmail.com>
2022-11-17 18:09:45 +0100

    .

--- js/moveSlider.js
+++ js/moveSlider.js
@@ -1,14 +1,13 @@
-function nextImage(){
+function nextImage() {
     //Get all images (slides) in carousel
     const elements = document.getElementsByClassName("slide");
     //Move image elements forward
     let auxElement = elements[0].outerHTML;
-    for (let index = 0; index < (elements.length-1); index++) {
-        elements[index].outerHTML = elements[index+1].outerHTML;
+    for (let index = 0; index < (elements.length - 1); index++) {
+        elements[index].outerHTML = elements[index + 1].outerHTML;
     }
-    elements[(elements.length-1)].outerHTML = auxElement;
-    
-    
+    elements[(elements.length - 1)].outerHTML = auxElement;
+
     //Get all descriptions
     const descriptions = document.getElementById("project-description").getElementsByTagName("p");
     //Move descriptions forward
@@ -20,39 +19,39 @@ function nextImage(){
         fin Si
       fin Bucle
     */
-    for (let index = 0; index <= (descriptions.length-1); index++) {
+    for (let index = 0; index <= (descriptions.length - 1); index++) {
         if (window.getComputedStyle(descriptions[index]).getPropertyValue('display') == "block") {
             descriptions[index].style.display = "none";
-            if (index == descriptions.length-1) {
+            if (index == descriptions.length - 1) {
                 descriptions[0].style.display = "block";
-            }else {
-                descriptions[index+1].style.display = "block";
+            } else {
+                descriptions[index + 1].style.display = "block";
             }
             break
         }
     }
 }
 
-function prevImage(){
+function prevImage() {
     //Get all images (slides) in carousel
     const elements = document.getElementsByClassName("slide");
     //Move elements backward
-    let auxElement = elements[(elements.length-1)].outerHTML;
-    for (let index = (elements.length-1); index > 0; index--) {
-        elements[index].outerHTML = elements[index-1].outerHTML;
+    let auxElement = elements[(elements.length - 1)].outerHTML;
+    for (let index = (elements.length - 1); index > 0; index--) {
+        elements[index].outerHTML = elements[index - 1].outerHTML;
     }
     elements[0].outerHTML = auxElement;
 
     //Get all descriptions
     const descriptions = document.getElementById("project-description").getElementsByTagName("p");
     //Move descriptions backward
-    for (let index = (descriptions.length-1); index >= 0; index--) {
+    for (let index = (descriptions.length - 1); index >= 0; index--) {
         if (window.getComputedStyle(descriptions[index]).getPropertyValue('display') == "block") {
             descriptions[index].style.display = "none";
             if (index == 0) {
-                descriptions[descriptions.length-1].style.display = "block";
-            }else {
-                descriptions[index-1].style.display = "block";
+                descriptions[descriptions.length - 1].style.display = "block";
+            } else {
+                descriptions[index - 1].style.display = "block";
             }
             break
         }
